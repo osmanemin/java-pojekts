73d164460ca7efe6252942dde203f90935f90104
Osman Emin Usta <osmaneminusta@gmail.com>
2019-06-24 20:21:57 +0300

    static ineer sınıflar

---
+++ Statik İneer Classlar/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Matematik.Alan alan = new Matematik.Alan();
+           
+        alan.alan_hesabi(4);
+        
+        Matematik.Alan.alan_hesabi(12);
+    }
+         
+    
+}
---
+++ Statik İneer Classlar/src/Matematik.java
@@ -0,0 +1,19 @@
+public class Matematik {
+    
+    private static double pi = Math.PI;
+    
+    public static class Alan{
+        
+        public static double alan;
+        
+        public static void alan_hesabi(double yari_cap){
+            alan = yari_cap * yari_cap * pi;
+            System.out.println("yaricap' i verilen dairenin alanı: " + alan );
+        }
+        
+    
+    
+    
+}
+    
+}
